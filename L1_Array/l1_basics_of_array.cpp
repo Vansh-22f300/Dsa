a7d2bf271b548ffef86d3bc662774a4b231c750f
Vansh-22f300 <22f3001851@ds.study.iitm.ac.in>
2025-02-19 19:02:11 +0530

    Day1

--- L1_Array/l1_basics_of_array.cpp
+++ L1_Array/l1_basics_of_array.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={1,2,3,4,5};
+    int arr[]={1,2,3,4,5};//initialization
     int n=sizeof(arr)/sizeof(int);
-    arr[0]=10;
+    arr[0]=10;//update
     for (int i=0;i<n;i++){
         cout<<" "<<arr[i];
     }
     cout<<endl;
+    return 0;
 }
